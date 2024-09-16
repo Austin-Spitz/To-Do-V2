79a5d5454bda1d4889f1ed95b5e14f0403f817a3
Austin Spitz <155266541+Austin-Spitz@users.noreply.github.com>
2024-09-15 20:11:43 -0500

    improved the UI / added logout icon / styled table

--- components/TaskInfo/TaskInfo.jsx
+++ components/TaskInfo/TaskInfo.jsx
@@ -5,6 +5,8 @@ import { useSession } from "next-auth/react";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { IoEllipsisHorizontalSharp } from "react-icons/io5";
 import Model2 from "../ModalComp";
+import { IoIosLogOut } from "react-icons/io";
+
 
 export default function TaskInfo({tasks=[], onTaskChange}) {
 
@@ -39,42 +41,57 @@ export default function TaskInfo({tasks=[], onTaskChange}) {
         onTaskChange(); // If there's an onTaskChange passed from a higher parent, call it
       };
 
+
+      function getPriority(priority){
+        switch(priority){
+            case 'High':
+                return 'red';
+            case 'Medium':
+                return 'yellow';
+            case 'Low':
+                return 'green'
+            default:
+                return 'red';
+        }
+      }
+
     return (
         <div className="bg-[url('https://cdn.pixabay.com/photo/2017/05/16/21/51/coffee-2319122_1280.jpg')] bg-center bg-no-repeat bg-cover w-full h-[100vh] overlay">
+            <h1>.</h1>
 
-            {/* <h1 className="session-name">Hello, {session?.user?.name}</h1> */}
-            <h1 className="header-name">Hello Andrew!</h1>
-            
-            <button onClick={() => signOut()}>Log out</button>
+            <main className="w-[82vw] h-[70vh] bg-[#fff5] mx-auto mt-40 backdrop-blur-[7px] shadow-[0_0.4rem_0.8rem_#0005] rounded-lg overflow-hidden">
+                <div className="items-center flex justify-center text-center float-right mr-[5rem] mt-[15px] gap-[3rem]">
+                    <Model2 onTaskChange={handleTaskChange}/>
+                    <IoIosLogOut size={30} className="cursor-pointer hover:text-red-600" onClick={() => signOut()}/>
+                </div>
 
-            <Model2 onTaskChange={handleTaskChange}/>
-            <div className="w-[80%] ml-[10%] mt-60 border-2 border-spacing-5 p-10 rounded-md overflow-y-scroll max-h-[400px]">
-                <table className="w-full my-6 border border-black border-separate border-spacing-0 text-sm font-normal font-sans table-fixed">
-                    <thead className="bg-red-600 text-white sticky top-0">
-                        <tr>
-                            <th className="bg-red-800 text-white sticky top-0">Description</th>
-                            <th className="bg-red-800 text-white sticky top-0">Status</th>
-                            <th className="bg-red-800 text-white sticky top-0">Due</th>
-                        </tr>
-                    </thead>
-                   <tbody className="bg-white divide-y divide-gray-200">
-    {tasks.length > 0 ? (
-        tasks.map((task) => (
-            <tr key={task._id}>
-                <td className="px-6 py-4 whitespace-nowrap">{task.taskD}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{task.taskStatus} <IoEllipsisHorizontalSharp className="cursor-pointer"/></td>
-                <td className="px-6 py-4 whitespace-nowrap">{task.taskDue} <FaRegTrashAlt onClick={() => handleDelete(task._id, task.taskD, task.taskStatus, task.taskDue)} className="cursor-pointer" /> </td>
-            </tr>
-        ))
-    ) : (
-        <tr>
-            <td colSpan="3" className="px-6 py-4 text-center">No tasks available</td>
-        </tr>
-    )}
-</tbody>
-                </table>
-            </div>
-
+                <section className="w-[95%] max-h-[calc(89%-1.6rem)] bg-[#fffb] my-3 mt-20 mx-auto rounded-md overflow-auto">
+                    <table className="w-full border-collapse">
+                        <thead>
+                            <tr>
+                                <th className="sticky text-left top-0 left-0 bg-[#E5BA73] text-[#000000d1] capitalize p-4 cursor-pointer">Description</th>
+                                <th className="sticky text-left top-0 left-0 bg-[#E5BA73] text-[#000000d1] capitalize p-4 cursor-pointer">Due Date</th>
+                                <th className="sticky text-left top-0 left-0 bg-[#E5BA73] text-[#000000d1] capitalize p-4 cursor-pointer">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {tasks.length > 0 ? (
+                                tasks.map((task) => (
+                                    <tr key={task._id} className="even:bg-[#0000000b] transition-[opacity,transform] duration-[0.5s] ease-in-out hover:bg-[#fff6]">
+                                        <td className="p-4">{task.taskD}</td>
+                                        <td className="p-4">{task.taskDue}</td>
+                                        <td className="p-4" style={{'color': getPriority(task.taskStatus)}}>{task.taskStatus}</td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="3" className="p-4 text-center">No tasks available</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </section>
+            </main>
         </div>
     )
 }
