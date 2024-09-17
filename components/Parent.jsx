10c5fc745e58fc1ea231c3573f645366a30804f0
Austin Spitz <155266541+Austin-Spitz@users.noreply.github.com>
2024-09-17 17:04:09 -0500

    Improved modal / added new favicon

--- components/Parent.jsx
+++ components/Parent.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, {useEffect, useState} from "react"
-import ModalComp from "../components/ModalComp"
+import ModalComp from "./AddTaskModal"
 import TaskInfo from "../components/TaskInfo/TaskInfo";
 
 
--- components/TaskInfo/TaskInfo.jsx
+++ components/TaskInfo/TaskInfo.jsx
@@ -2,12 +2,8 @@
 import { signOut } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
-import { FaRegTrashAlt } from "react-icons/fa";
-import { IoEllipsisHorizontalSharp } from "react-icons/io5";
-import Model2 from "../ModalComp";
+import Model2 from "../AddTaskModal";
 import { IoIosLogOut } from "react-icons/io";
-
-
 export default function TaskInfo({tasks=[], onTaskChange}) {
 
     console.log("Tasks: ", tasks);
@@ -41,17 +37,30 @@ export default function TaskInfo({tasks=[], onTaskChange}) {
         onTaskChange(); // If there's an onTaskChange passed from a higher parent, call it
       };
 
-
-      function getPriority(priority){
-        switch(priority){
+      const getPriorityBg = (status) => {
+        switch(status){
             case 'High':
-                return 'red';
+                return '#d893a3';
             case 'Medium':
-                return 'yellow';
+                return '#ebc474';
             case 'Low':
-                return 'green'
+                return '#86e49d';
             default:
-                return 'red';
+                return 'white';
+        }
+      }
+
+      const getPriorityColor = (status) => {
+
+        switch(status){
+            case 'High':
+                return '#b30021';
+            case 'Medium':
+                return '#df922e';
+            case 'Low':
+                return '#006b21';
+            default:
+                return 'black';
         }
       }
 
@@ -61,36 +70,44 @@ export default function TaskInfo({tasks=[], onTaskChange}) {
 
             <main className="w-[82vw] h-[70vh] bg-[#fff5] mx-auto mt-40 backdrop-blur-[7px] shadow-[0_0.4rem_0.8rem_#0005] rounded-lg overflow-hidden">
                 <div className="items-center flex justify-center text-center float-right mr-[5rem] mt-[15px] gap-[3rem]">
-                    <Model2 onTaskChange={handleTaskChange}/>
                     <IoIosLogOut size={30} className="cursor-pointer hover:text-red-600" onClick={() => signOut()}/>
                 </div>
 
-                <section className="w-[95%] max-h-[calc(89%-1.6rem)] bg-[#fffb] my-3 mt-20 mx-auto rounded-md overflow-auto">
+                <section className="w-[95%] h-[23rem] bg-[#fffb] my-3 mt-20 mx-auto rounded-md overflow-y-scroll">
                     <table className="w-full border-collapse">
-                        <thead>
-                            <tr>
-                                <th className="sticky text-left top-0 left-0 bg-[#E5BA73] text-[#000000d1] capitalize p-4 cursor-pointer">Description</th>
-                                <th className="sticky text-left top-0 left-0 bg-[#E5BA73] text-[#000000d1] capitalize p-4 cursor-pointer">Due Date</th>
-                                <th className="sticky text-left top-0 left-0 bg-[#E5BA73] text-[#000000d1] capitalize p-4 cursor-pointer">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {tasks.length > 0 ? (
-                                tasks.map((task) => (
-                                    <tr key={task._id} className="even:bg-[#0000000b] transition-[opacity,transform] duration-[0.5s] ease-in-out hover:bg-[#fff6]">
-                                        <td className="p-4">{task.taskD}</td>
-                                        <td className="p-4">{task.taskDue}</td>
-                                        <td className="p-4" style={{'color': getPriority(task.taskStatus)}}>{task.taskStatus}</td>
-                                    </tr>
-                                ))
-                            ) : (
-                                <tr>
-                                    <td colSpan="3" className="p-4 text-center">No tasks available</td>
-                                </tr>
-                            )}
-                        </tbody>
+                    <thead>
+    <tr>
+        <th className="sticky text-left top-0 left-0 bg-[#ffa938] text-[#000000d1] capitalize p-4 cursor-pointer break-words">
+            Description
+        </th>
+        <th className="sticky text-left top-0 left-0 bg-[#ffa938] text-[#000000d1] capitalize p-4 cursor-pointer break-words">
+            Due Date
+        </th>
+        <th className="sticky text-left top-0 left-0 bg-[#ffa938] text-[#000000d1] capitalize p-4 cursor-pointer break-words">
+            Status
+        </th>
+    </tr>
+</thead>
+<tbody>
+    {tasks.length > 0 ? (
+        tasks.map((task,key) => (
+            <tr key={task._id} className="even:bg-[#0000000b] transition-[opacity,transform] duration-[0.5s] ease-in-out hover:bg-[#fff6]">
+                <td className="p-4 break-words">{task.taskD}</td>
+                <td className="p-4 break-words">{task.taskDue}</td>
+                <td className="p-4 break-words status" style={{'color': getPriorityColor(task.taskStatus), 'backgroundColor': getPriorityBg(task.taskStatus)}}>{task.taskStatus} 
+                </td>
+            </tr>
+        ))
+    ) : (
+        <tr>
+            <td colSpan="3" className="p-4 text-center break-words">No tasks available</td>
+        </tr>
+    )}
+</tbody>
+
                     </table>
                 </section>
+                <Model2 onTaskChange={handleTaskChange}/>
             </main>
         </div>
     )
